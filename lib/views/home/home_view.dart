e68be89575e2ddc2b36b0ee85e6b915eb30a2551
Mauricio Posada <mauricioposadamurillo@gmail.com>
2025-05-28 13:39:36 -0500

    feat(home): refactor home view with new components and spacing
    - Extract HomeBody and HomeAppBar into separate components for better maintainability
    - Add space extension for easier widget spacing
    - Update progress indicator with custom styling and progress value
    - Use app colors and strings constants for consistency

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tasks/extensions/space_ext.dart';
+import 'package:tasks/utils/app_colors.dart';
+import 'package:tasks/utils/app_strings.dart';
 import 'package:tasks/views/home/widget/add_task_button.dart';
 
 class HomeView extends StatefulWidget {
@@ -16,29 +19,69 @@ class _HomeViewState extends State<HomeView> {
 
       floatingActionButton: AddTaskButton(),
 
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 60),
-              width: double.infinity,
-              height: 100,
-              color: Colors.red,
-              child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 25,
-                    width: 25,
-                    child: CircularProgressIndicator(),
-                  )
-                ],
-              ),
+      body: HomeBody(),
+    );
+  }
+}
+
+class HomeBody extends StatelessWidget {
+  const HomeBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //
+    TextTheme textTheme = Theme.of(context).textTheme;
+
+    return SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+        children: [
+          HomeAppBar(textTheme: textTheme),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({
+    super.key,
+    required this.textTheme,
+  });
+
+  final TextTheme textTheme;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 60),
+      width: double.infinity,
+      height: 100,
+      color: const Color.fromARGB(16, 244, 67, 54),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 30,
+            width: 30,
+            child: CircularProgressIndicator(
+              value: 1 / 3,
+              backgroundColor: Colors.grey,
+              valueColor: AlwaysStoppedAnimation(AppColors.primaryColor),
             ),
-          ],
-        ),
+          ),
+          25.w,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(AppStrings.mainTitle, style: textTheme.displayLarge),
+              3.h,
+              Text("1 of 3", style: textTheme.titleMedium),
+            ],
+          ),
+        ],
       ),
     );
   }
